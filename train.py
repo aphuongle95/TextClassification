15d18ea6b66c0503500e782309726b586ba79662
annie <aphuongle95@gmail.com>
2023-08-08 00:34:04 +0200

    format

--- train.py
+++ train.py
@@ -2,7 +2,15 @@
 # coding: utf-8
 
 
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer, DataCollatorWithPadding, TextClassificationPipeline, EarlyStoppingCallback
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    TrainingArguments,
+    Trainer,
+    DataCollatorWithPadding,
+    TextClassificationPipeline,
+    EarlyStoppingCallback,
+)
 from datasets import load_dataset, load_from_disk, load_metric
 import numpy as np
 
@@ -12,76 +20,60 @@ from utils import strip_text, compute_metrics
 def train_with_random_search(tokenized_dataset, model, tokenizer, model_name):
     import wandb
 
-    sweep_config = {
-        'method': 'random'
-    }
-
-
+    sweep_config = {"method": "random"}
 
     parameters_dict = {
-        'epochs': {
-            'value': 5
-            },
-        'batch_size': {
-            'values': [8, 16]
-            },
-        'learning_rate': {
-            'distribution': 'log_uniform_values',
-            'min': 1e-5,
-            'max': 1e-3
+        "epochs": {"value": 5},
+        "batch_size": {"values": [8, 16]},
+        "learning_rate": {
+            "distribution": "log_uniform_values",
+            "min": 1e-5,
+            "max": 1e-3,
         },
-        'weight_decay': {
-            'values': [0.001, 0.01, 0.1, 0.5]
-        },
+        "weight_decay": {"values": [0.001, 0.01, 0.1, 0.5]},
     }
 
-
-    sweep_config['parameters'] = parameters_dict
-    sweep_id = wandb.sweep(sweep_config, project='seq-classification-sweeps')
+    sweep_config["parameters"] = parameters_dict
+    sweep_id = wandb.sweep(sweep_config, project="seq-classification-sweeps")
 
     def model_init():
         return AutoModelForSequenceClassification.from_pretrained(
-            model_name, return_dict=True)
+            model_name, return_dict=True
+        )
 
     with wandb.init(config=config):
-        
         config = wandb.config
 
-
-        
         training_args = TrainingArguments(
-            output_dir='seq-classification-sweeps',
-            report_to='wandb',
+            output_dir="seq-classification-sweeps",
+            report_to="wandb",
             num_train_epochs=config.epochs,
             learning_rate=config.learning_rate,
             weight_decay=config.weight_decay,
             per_device_train_batch_size=config.batch_size,
             per_device_eval_batch_size=8,
-        logging_strategy='epoch',
-        evaluation_strategy='epoch',
-        save_strategy='epoch',
-        load_best_model_at_end=True,
-
+            logging_strategy="epoch",
+            evaluation_strategy="epoch",
+            save_strategy="epoch",
+            load_best_model_at_end=True,
             # remove_unused_columns=False,
             # fp16=True
         )
 
-
-        
         trainer = Trainer(
             # model,
             model_init=model_init,
             args=training_args,
-            train_dataset=tokenized_dataset['train'],
-            eval_dataset=tokenized_dataset['test'],
-            compute_metrics=compute_metrics
+            train_dataset=tokenized_dataset["train"],
+            eval_dataset=tokenized_dataset["test"],
+            compute_metrics=compute_metrics,
         )
-
 
         # start training loop
         trainer.train()
 
     wandb.agent(sweep_id, train, count=20)
+
 
 def train_with_hyperparameters(tokenized_dataset, model, tokenizer, model_name):
     training_args = TrainingArguments(
@@ -91,12 +83,11 @@ def train_with_hyperparameters(tokenized_dataset, model, tokenizer, model_name):
         per_device_eval_batch_size=16,
         num_train_epochs=10,
         weight_decay=0.1,
-        logging_strategy='epoch',
-        evaluation_strategy='epoch',
-        save_strategy='epoch',
+        logging_strategy="epoch",
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
         load_best_model_at_end=True,
     )
-
 
     trainer = Trainer(
         model=model,
@@ -105,21 +96,19 @@ def train_with_hyperparameters(tokenized_dataset, model, tokenizer, model_name):
         eval_dataset=tokenized_dataset["test"],
         tokenizer=tokenizer,
         compute_metrics=compute_metrics,
-        callbacks = [EarlyStoppingCallback(early_stopping_patience=2)],
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=2)],
         # data_collator=data_collator,
     )
 
     trainer.train()
-    trainer.save_model("model/"+model_name)
-    
-def train(model_name: str, random_search: bool):
+    trainer.save_model("model/" + model_name)
 
-    model = AutoModelForSequenceClassification.from_pretrained(model_name , num_labels=2)
+
+def train(model_name: str, random_search: bool):
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-
-    dataset = load_dataset('csv', data_files={'negative_data.csv', 'positive_data.csv'})
-
+    dataset = load_dataset("csv", data_files={"negative_data.csv", "positive_data.csv"})
 
     dataset.shuffle(seeds=20)
 
@@ -127,21 +116,20 @@ def train(model_name: str, random_search: bool):
     dataset = dataset.rename_column("Review", "text")
     dataset = dataset.rename_column("Label", "label")
 
-
-    dataset = dataset['train']
+    dataset = dataset["train"]
 
     dataset = dataset.map(strip_text)
     dataset = dataset.train_test_split(test_size=0.1)
 
-    tokenize_function = lambda examples: tokenizer(examples["text"], padding='max_length', truncation=True, max_length=256)
+    tokenize_function = lambda examples: tokenizer(
+        examples["text"], padding="max_length", truncation=True, max_length=256
+    )
 
-    tokenized_dataset = dataset.map(tokenize_function, batched=True, remove_columns='text')
+    tokenized_dataset = dataset.map(
+        tokenize_function, batched=True, remove_columns="text"
+    )
 
-    
-    
     if random_search:
         train_with_random_search(tokenized_dataset, model, tokenizer, model_name)
     else:
         train_with_hyperparameters(tokenized_dataset, model, tokenizer, model_name)
-
-    
--- utils.py
+++ utils.py
@@ -1,12 +1,16 @@
 import numpy as np
 from datasets import load_dataset, load_from_disk, load_metric
 
+
 def strip_text(example):
     example["text"] = example["text"].strip()
     return example
 
+
 metric = load_metric("accuracy")
-def compute_metrics(eval_pred) :
+
+
+def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
